c3c719353813ebefa86f78061c7c5618f65e0089
AyhamFuqha1 <ayhamfuqha55@gmail.com>
2025-11-01 01:05:39 +0200

    second

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "tala moammer" );
-=======
-        System.out.println( "Hello World!" );
->>>>>>> 8c55163660cefb578850f231db3fb920bd084e07
+        System.out.println( "tala moamer" );
     }
 }
